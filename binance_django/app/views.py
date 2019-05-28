3ca303939257f7a1e57bd3b292381808453b5d8a
Aleksei Kharitonov <northdell@gmail.com>
2019-05-28 16:49:39 +0300

    init binance_client

--- binance_django/app/views.py
+++ binance_django/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from binance.client import Client
 
-# Create your views here.
+
+def index(request):
+    api_key = 'UlkP3TPyMyicFkLzs5naHx9gWvi7W39A3CIdHnWl0yGLG5Yoow9y8SmNn7XFDsrS'
+    secret_key = 'IvXFouzzfA149RMYuu9FryUt7N3Bj4bVzZbd7A9JTxNM0tWzCigEvukpy5CumbQ3'
+
+    client = Client(api_key, secret_key)
+    status = client.get_system_status()
+
+    return render(request, 'app/index.html', context={'status': status})
