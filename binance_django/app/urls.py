9d444c47a4531ce918e8c3c5a9ca99f087be0cc8
Aleksei Kharitonov <northdell@gmail.com>
2019-05-29 23:32:18 +0300

    add router for switch refresh

--- binance_django/app/urls.py
+++ binance_django/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='main-page')
+    path('', index, name='main-page'),
+    path('switch/', switch, name='switch')
 ]
